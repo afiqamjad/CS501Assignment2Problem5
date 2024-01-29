f18f3945dbb0942303aad07ba3cb360c5111fa44
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-01-29 15:47:49 -0500

    Initialized the app with the EditText and a Button.

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "[AfiqAmjad]Assignment2Problem5"
+include(":app")
+ 
