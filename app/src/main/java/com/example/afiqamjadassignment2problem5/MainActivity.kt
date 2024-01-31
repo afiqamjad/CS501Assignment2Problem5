d89e5411b859c70e1cbf6cc66279ade8698abba4
afiqamjad <afiqk@bu.edu>
2024-01-31 15:08:10 -0500

    Added sqrt functionality to equals button but doesn't work yet.

--- app/src/main/java/com/example/afiqamjadassignment2problem5/MainActivity.kt
+++ app/src/main/java/com/example/afiqamjadassignment2problem5/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,14 @@ class MainActivity : AppCompatActivity() {
         val seven = findViewById<Button>(R.id.num7)
         val eight = findViewById<Button>(R.id.num8)
         val nine = findViewById<Button>(R.id.num9)
+        val zero = findViewById<Button>(R.id.num0)
+        val dot = findViewById<Button>(R.id.numdot)
+        val plus = findViewById<Button>(R.id.plus)
+        val times = findViewById<Button>(R.id.times)
+        val minus = findViewById<Button>(R.id.minus)
+        val divide = findViewById<Button>(R.id.divide)
+        val sqrt = findViewById<Button>(R.id.sqrt)
+        val equals = findViewById<Button>(R.id.equals)
 
         val text = findViewById<EditText>(R.id.editText)
 
@@ -48,6 +57,43 @@ class MainActivity : AppCompatActivity() {
         }
         nine.setOnClickListener{
             text.append("9")
+        }
+        zero.setOnClickListener{
+            text.append("0")
+        }
+        dot.setOnClickListener {
+            text.append(".")
+        }
+        plus.setOnClickListener{
+            text.append("+")
+        }
+        times.setOnClickListener{
+            text.append("*")
+        }
+        minus.setOnClickListener{
+            text.append("-")
+        }
+        divide.setOnClickListener{
+            text.append("/")
+        }
+        sqrt.setOnClickListener{
+            text.append("√")
+        }
+        equals.setOnClickListener{
+            val wholeThing = text.text.toString()
+            if (wholeThing != "") {
+                val splitted = wholeThing.split("+", "-", "*", "/", "√")
+                if (splitted[0] == "√") {
+                    if (splitted.size > 1) {
+                        val num = splitted[1].toDouble()
+                        val ans = sqrt(num)
+                        text.setText(ans.toString())
+                    } else {
+                        text.error = "Bro"
+                    }
+                }
+            }
+
         }
 
     }
